4d8438c776d6619781979d39c80ebc635c92f5c8
Emilia Rutkowska <emilia.weronika.rutkowska@gmail.com>
2020-03-24 13:03:08 +0100

    Fix import

--- examples/example_lab3_task5.py
+++ examples/example_lab3_task5.py
@@ -1,4 +1,4 @@
-from lab3_task5 import min_spanning_tree_Kruskal
+from tasks.lab3_task5 import min_spanning_tree_Kruskal
 
 list_of_values = [[7,2,3],[7,7,8],[8,0,7],[8,1,2],[9,3,4],[10,5,4],[11,1,7],[14,3,5],[1,7,6],[2,8,2],[2,6,5],[4,0,1],[4,2,5],[6,8,6]]
 mst = min_spanning_tree_Kruskal(list_of_values, 9)
