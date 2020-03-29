206ab1f93afb4a14b6125d6e30b76b4d5fd6b5f6
Emilia Rutkowska <emilia.weronika.rutkowska@gmail.com>
2020-03-29 19:31:56 +0200

    Fix function create_adjacency_list

--- tasks/lab2_task1.py
+++ tasks/lab2_task1.py
@@ -6,61 +6,71 @@ def degree_seq(A, n):
         Funkcja degree_seq- sprawdza czy lista stopni wierzcholkow grafu (lista ilosci krawedzi
         wychodzacych z poszczegolnych wierzcholkow) jest ciagiem graficznym , zwraca True/False
     '''
-    A_sorted = sorted(A, reverse=True)
-    print(A_sorted)
-    # warunek na nieparzysta liczbe nieparzystych cyfr w A
+    A_sorted = sorted(A, reverse = True)
+    print (A_sorted)
+    # warunek na nieparzysta liczbe nieparzystych cyfr w A 
     if sum(i%2 for i in A_sorted)%2:
-        return False
+        return False 
+    
     while True:
-        if max(A_sorted) == min(A_sorted) == 0:
-            return True
-        elif A_sorted[0] < 0 or A_sorted[0] >= n or min(A_sorted) < 0:
+        if max(A_sorted) == min(A_sorted) == 0: 
+            print("Returning becase all are 0 ({})".format(A_sorted))
+            return True 
+        elif A_sorted[0] < 0 or A_sorted[0] >= n or max(A_sorted) < 0:
             return False
+        else:   
 
-        for i in range(1, A_sorted[0] + 1):
-            A_sorted[i] = A_sorted[i] - 1
+            for i in range(1, A_sorted[0] + 1):
+                if A_sorted[i] >= 1:
+                    A_sorted[i] = A_sorted[i] - 1
+                else:
+                    return False
 
-        A_sorted[0] = 0
-        A_sorted = sorted(A_sorted, reverse=True)
+            A_sorted[0] = 0 
+            A_sorted = sorted(A_sorted, reverse = True)
+            
 
 def create_adjacency_list(A):
     '''
         Funkcja tworzaca liste sasiedztwa na podstawie wczytanego ciagu graficznego
         zwraca liste list, podlisty zawieraja pary wierzcholkow polaczonych krawedzia
-        bez powtorzen tz krawedz [1,3] jest tozsama z [3,1]
+        bez powtorzen tz krawedz [1,3] jest tozsama z [3,1].
+        Zawiera sortowanie dobieranych wezlow po ilosci dostepnych slotow, 
+        minimalizuje szane ze niemoznaby znalezc wezla, z ktorym moznaby polaczyc aktualny wezel 
     '''
-    edges = int(sum(A)/2)
-    A = sorted(A, reverse=True)
+    edges = int(sum(A)/2) 
+    A = sorted(A, reverse = True)
 
-    #stworz liste list, podlista zawiera: nr wierzcholka, stopien wierzcholka,
+    #stworz liste list, podlista zawiera: nr wierzcholka, stopien wierzcholka, 
     #licznik odnotowanych polaczen (krawedzi) z danego wierzcholka
-    nodes_status = [[node_idx, grade, 0] for node_idx, grade in enumerate(A)]
+    nodes_status = [[node_idx, grade, 0] for node_idx, grade in enumerate(A) ]
     print(nodes_status)
-
+ 
     adjacency_list = set()
 
-    for _ in range(edges):
+    print("Will try to set up {} edges".format(edges))
+    # na wszelki wypadek sortuje liste malejaco po ilosci dostepnych slotow
+    nodes_status = sorted(nodes_status, key=lambda x: x[1], reverse=True)
+    for i in range(edges):
+    #zostaw w liscie wierzcholki, z ktorych mozna wyprowadzic przynajmniej jedna krawedz 
+        nodes_status = [node for node in nodes_status if int(node[1]) > int(node[2])]
 
-    #usun wszyskie wierzcholki z listy dla ktorych ilosc polaczen z wierzcholka
-    #(grades) rowna jest licznikowi wykorzystanych polaczen
-        for node in nodes_status:
-            if node[1] == node[2]:
-                nodes_status.remove(node)
-
-        for node in nodes_status[1:]:
+        for node in sorted(nodes_status[1:], key=lambda x: (x[1] - x[2], x[1]), reverse=True):
             #set zawierajacy numer wierzcholka pierwszego z nodes_status i numer kolejnego wierzcholka z nodes_status
-            tmp_set = set([nodes_status[0][0], node[0]])
-
-            if tmp_set not in adjacency_list:
-                adjacency_list.add(frozenset(tmp_set))
+            maybe_edge = set([nodes_status[0][0], node[0]])
+    
+            if maybe_edge not in adjacency_list:
+                adjacency_list.add(frozenset(maybe_edge))
                 nodes_status[0][2] += 1
                 node[2] += 1
                 break
+        else:
+            print("This node is already connected to all nodes:", nodes_status[0])
+            print(nodes_status)
 
-        nodes_status.sort(key=lambda x: x[1] - x[2], reverse=True) #posortuj po liczbie dostepnych krawedzi
-
+  
     result_list = [list(f_set) for f_set in adjacency_list]
-    result_list.sort()
+    result_list.sort(key=lambda x: (x[0], x[1]))
 
     return result_list
 
