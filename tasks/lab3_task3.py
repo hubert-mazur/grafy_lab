9f635463fe011d8d1c4b0d389856992462b4356b
hubert-mazur <hiusvii@gmail.com>
2020-05-03 11:44:04 +0200

    update

--- tasks/lab3_task3.py
+++ tasks/lab3_task3.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3.7
 
 from utils.dijkstra import dijkstra
-from tasks.lab3_task2 import lab3_task2
 
 
-def lab3_task3(numberOfNodes, numberOfEdges):
-    ret = lab3_task2(numberOfNodes, numberOfEdges)
-
-    distanceMatrix = [[] for i in range(numberOfNodes)]
+def lab3_task3(ret):
+    numberOfNodes = len(ret[0].nodeList)
+    distanceMatrix = [[] for _ in range(numberOfNodes)]
 
     for i in range(numberOfNodes):
         distanceMatrix[i].extend(*[dijkstra(ret[0], ret[1], i)[0].values()])
