d7e2b4bbf4bd3e63fe51ab8298a690c81fa2f8f4
Emilia Rutkowska <emilia.weronika.rutkowska@gmail.com>
2020-05-04 15:23:35 +0200

    Last changes

--- examples/example_lab3_task5.py
+++ examples/example_lab3_task5.py
@@ -1,8 +1,17 @@
-from lab3_task5 import min_spanning_tree_Kruskal
+from tasks.lab3_task5 import min_spanning_tree_Kruskal, print_graph
 
-list_of_values = [[7,2,3],[7,7,8],[8,0,7],[8,1,2],[9,3,4],[10,5,4],[11,1,7],[14,3,5],[1,7,6],[2,8,2],[2,6,5],[4,0,1],[4,2,5],[6,8,6]]
+list_of_values = [[7,2,3],[7,7,8],[8,0,7],[8,1,2],[9,3,4],[10,5,4],[11,1,7],[14,3,5],[1,7,6],[2,8,2],[2,6,5],[4,0,1],[4,2,5],[6,8,6],[2,7,9],[3,1,9]]
+# list_of_values= [[2,1,2],[3,2,3],[6,3,4],[5,4,1],[1,0,4],[4,0,2],[1,0,1]]
+result = [(k[1],k[2],k[0]) for k in list_of_values]
+
 mst = min_spanning_tree_Kruskal(list_of_values, 9)
-
 print("Printing minimum spanning tree")
 for weight, source, destination in sorted(mst, key=lambda edge: edge[1]):
     print("Edge from {} to {} (weight: {})".format(source, destination, weight))
+
+print_graph(result)
+
+
+
+
+
--- tasks/lab3_task5.py
+++ tasks/lab3_task5.py
@@ -1,10 +1,13 @@
 from copy import deepcopy
+import matplotlib.pyplot as plt
+import networkx as nx
+import math
 
 '''
-Wiadomo, że do drzewa T będą należały wszystkie wierzchołki. Początkowo nie są połączone (należą do
-różnych drzew). Sortujemy krawędzie grafu G według niemalejących wag. Dla każdej krawędzi (u, v)
-od najmniejszej wagi: jeżeli dodanie krawędzi (u, v) nie spowoduje powstania cyklu (czyli: krawędź
-(u, v) łączy różne drzewa), to dodajemy ją do T.
+Wiadomo, ze do drzewa T beda nalezaly wszystkie wierzcholki. Poczatkowo nie sa polaczone (naleza do
+roznych drzew). Sortujemy krawedzie grafu G wedlug niemalejacych wag. Dla kazdej krawedzi (u, v)
+od najmniejszej wagi: jezeli dodanie krawedzi (u, v) nie spowoduje powstania cyklu (czyli: krawedz
+(u, v) laczy rozne drzewa), to dodajemy ja do T.
 '''
 
 def min_spanning_tree_Kruskal(G_list, v): #G_list[[weight,source,dest]] -> gdzie source,dest to wierzcholki krawedzi 
@@ -13,15 +16,13 @@ def min_spanning_tree_Kruskal(G_list, v): #G_list[[weight,source,dest]] -> gdzie
     trees = [[k] for k in range(v)]
     G_list.sort(key = lambda x: x[0])
 
-    for node in range(len(G_list)): 
+    for edge in range(len(G_list)): 
         weight, source, destination = deepcopy(G_list[0])
-        print("Handling edge from {} to {} with weight {}".format(source, destination, weight))
         G_list.pop(0)
 
         for subtree in trees: 
             if source in subtree and destination in subtree:
-                print("{} and {} already in {}".format(source, destination, subtree))
-                continue
+                break
             elif source in subtree:
                 MST.append([weight, source, destination]) # append edge
                 for other_subtree in trees: 
@@ -29,10 +30,32 @@ def min_spanning_tree_Kruskal(G_list, v): #G_list[[weight,source,dest]] -> gdzie
                         idx_dest = trees.index(other_subtree)
 
                 idx_source = trees.index(subtree)
-                trees[idx_source].extend(trees[idx_dest])
+                trees[idx_source].extend(trees[idx_dest]) 
                 trees[idx_dest] = []
         
                 break
-
     return MST
+
+
+def print_graph(edges_list): 
+
+    G = nx.Graph()
+    G.add_weighted_edges_from(edges_list)
+
+    alpha = 2 * math.pi / G.number_of_nodes()
+    x_0, y_0, radius = 20, 20, 8
+    
+    positions = {}
+    for i in range(G.number_of_nodes()):
+            positions.update(
+                {(i): (x_0 + radius * math.cos(i * alpha), y_0 + radius * math.sin(i * alpha))})
+    
+    plt.axis('off')
+    plt.tight_layout()
+    graph_labels = nx.get_edge_attributes(G,'weight')
+    nx.draw_networkx_nodes(G, pos=positions)
+    nx.draw_networkx_edges(G, pos=positions)
+    nx.draw_networkx_labels(G, pos=positions)
+    nx.draw_networkx_edge_labels(G, pos=positions, edge_labels=graph_labels ) 
+    plt.show() 
             
